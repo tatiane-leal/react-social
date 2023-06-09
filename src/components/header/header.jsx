df2907933fa281c167c9dbb874cbf6d90bb280e2
Tatiane Sala <tatianedleal@gmail.com>
2023-06-08 22:59:33 -0400

    add logo for header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,8 +1,10 @@
 import styles from "./header.module.css";
+import logo from "./../../../assets/images/logo.svg";
 
 export function Header() {
   return (
     <header className={styles.header}>
+      <img src={logo} alt="Logotipo" />
       <strong>React Social Feed</strong>
     </header>
   );
