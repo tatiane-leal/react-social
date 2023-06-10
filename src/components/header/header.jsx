9c89680bfeb4f40bd3b6534d3ef924507ea3c994
Tatiane Sala <tatianedleal@gmail.com>
2023-06-09 21:21:36 -0400

    remove old logo

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,8 @@
 import styles from "./Header.module.css";
-import logo from "./../../../assets/images/logo.svg";
 
 export function Header() {
   return (
     <header className={styles.header}>
-      <img src={logo} alt="Logotipo" />
       <strong>React Social Feed</strong>
     </header>
   );
