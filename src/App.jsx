1e3cfffcbb3e6dd8647d9cc1fadf2f4cc9cce014
Tatiane Sala <tatianedleal@gmail.com>
2023-06-30 15:57:17 -0400

    add posts array

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,37 @@ import styles from "./App.module.css";
 import { HeaderC } from "./components/HeaderC/HeaderC";
 import { SidebarC } from "./components/SidebarC/SidebarC";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/tatiane-leal.png",
+      name: "Tatiane Sala",
+      role: "Front End Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galera" },
+      { type: "paragraph", content: "Eis um novo projeto" },
+      { type: "link", content: "linkaqui" },
+    ],
+    publishedAt: new Date("2022-05-30 21:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/gabriel-leal.png",
+      name: "Gabriel Sala",
+      role: "Front End Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Ola" },
+      { type: "paragraph", content: "um novo post" },
+      { type: "link", content: "outrolink" },
+    ],
+    publishedAt: new Date("2022-05-20 20:00:00"),
+  },
+];
+
 export function App() {
   return (
     <>
@@ -13,9 +44,15 @@ export function App() {
         <SidebarC />
 
         <main>
-          <Post />
-          <Post />
-          <Post />
+          {posts.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </>
